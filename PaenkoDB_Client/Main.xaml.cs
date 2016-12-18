e969f4adfcfc60701ce9f151fe8da0f8f3328da7
Florian Hanko <FlorianHanko@hotmail.com>
2016-12-18 14:36:37 +0100

    Added file posting via drag and drop

--- PaenkoDB_Client/Main.xaml.cs
+++ PaenkoDB_Client/Main.xaml.cs
@@ -31,9 +31,6 @@ namespace PaenkoDB_Client
             InitializeComponent();
             OpenNode = target;
             MainContent = new TextRange(Output.Document.ContentStart, Output.Document.ContentEnd);
-            BtnSend.Click += (o, e) => PostPutFile((bool)(tglMode.IsChecked) ? PaenkoDB.PaenkoDB.Method.Put : PaenkoDB.PaenkoDB.Method.Post);
-            BtnGet.Click += (o, e) => GetFile();
-            BtnDelete.Click += (o, e) => DeleteDocument();
             MainContent.Text = $"" +
                 $"Paenko Node {OpenNode.NodeLocation.ip} on port {OpenNode.NodeLocation.HttpPort} \r\n" +
                 $"Country {OpenNode.NodeLocation.countryCode} : {OpenNode.NodeLocation.country}\r\n" +
@@ -42,29 +39,62 @@ namespace PaenkoDB_Client
                 $"Zipcode {OpenNode.NodeLocation.zip}\r\n" +
                 $"Timezone {OpenNode.NodeLocation.timezone}\r\n" +
                 $"";
+            UpdateExplorer();
+            FileExplorer.Drop += (o,e) => ExplorerDrop(e);
+            FileExplorer.MouseDoubleClick += (o, e) => ExplorerClick((Img)((ListBox)o).SelectedItem, e);
         }
 
-        void PostPutFile(PaenkoDB.PaenkoDB.Method m)
+        void ExplorerClick(Img o, MouseButtonEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
-            var doc = PaenkoDocument.Open(ofd.FileName, Init.User, Init.Password);
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                DeleteDocument(o.Str);
+            }
+            else if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                GetFile(o.Str);
+            }
+            UpdateExplorer();
+        }
+
+        void ExplorerDrop(DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    PostPutFile((bool)(tglMode.IsChecked) ? PaenkoDB.PaenkoDB.Method.Put : PaenkoDB.PaenkoDB.Method.Post, file);
+                }
+                UpdateExplorer();
+            }
+        }
+
+        void UpdateExplorer()
+        {
+            FileExplorer.Items.Clear();
+            foreach (string s in GetKeys())
+            {
+                Image I = new Image() { Source = new BitmapImage(new Uri(@"C:\Users\Flori\Documents\DocumentIcon.png", UriKind.RelativeOrAbsolute)), Width = 20, Height=20 };
+                FileExplorer.Items.Add(new Img(s, I));
+            }
+        }
+
+        void PostPutFile(PaenkoDB.PaenkoDB.Method m, string filename)
+        {
+            var doc = PaenkoDocument.Open(filename, Init.User, Init.Password);
             if (m == PaenkoDB.PaenkoDB.Method.Put)
             {
-                KeySelection ks = new KeySelection(GetKeys());
+                KeySelection ks = new KeySelection(GetKeys()); 
                 ks.ShowDialog();
                 if (ks.Selection == "") return;
                 doc.id = ks.Selection;
             }
             Database.PostDocument(OpenNode, doc, m);
         }
-
-        void GetFile()
+        void GetFile(string fileid)
         {
-            KeySelection ks = new KeySelection(GetKeys());
-            ks.ShowDialog();
-            if (ks.Selection == "") return;
-            var response = Database.GetDocument(OpenNode, ks.Selection);
+            var response = Database.GetDocument(OpenNode, fileid);
             int fileCount = 0;
             byte[] buffer;
             while (File.Exists($"Document{fileCount}")) { fileCount++; }
@@ -86,17 +116,22 @@ namespace PaenkoDB_Client
             Process.Start($"Document{fileCount - 1}");
         }
 
-        void DeleteDocument()
+        void DeleteDocument(string fileid)
         {
-            KeySelection ks = new KeySelection(GetKeys());
-            ks.ShowDialog();
-            if (ks.Selection == "") return;
-            Database.DeleteDocument(OpenNode, ks.Selection);
+            Database.DeleteDocument(OpenNode, fileid);
         }
 
         List<string> GetKeys()
         {
             return Database.GetKeys(OpenNode);
         }
+    }
+
+    public class Img
+    {
+        public Img(string value, Image img) { Str = value; Image = img; }
+        public string Str { get; set; }
+        public Image Image { get; set; }
+        public string ShowStr { get { return Str.Substring(0, 5); } }
     }
 }
